5100e062b435a79105864d21cd0169e4c4030b66
Abraham V G <60133406+abrahamvg@users.noreply.github.com>
2024-03-02 17:24:35 +0530

    Some minor changes

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def scrape_website():
         heading = soup.find('title').get_text()
 
         # Define selectors to remove specific elements
-        selectors = ['logo','byline_action','logo','timestamp', 'trending', 'widget', 'footer', 'new_', 'header', 'nav', 'slick', 'slider', 'personalise', 'visualstories', 'popup', 'subscribe', 'meta', 'caption', 'rhs', 'recommendation', 'comment', 'share']
+        selectors = ['info','inSideInd','hidden','report','btmFooter','toaster','c-compact-river','next','social-buttons','socialButtons','social','logo','byline_action','logo','timestamp', 'trending', 'widget', 'footer', 'new_', 'header', 'nav', 'slick', 'slider', 'personalise', 'visualstories', 'popup', 'subscribe', 'meta', 'caption', 'rhs', 'recommendation', 'comment', 'share', 'newsletter']
 
         # Remove elements matching selectors
         # Remove elements matching selectors
@@ -38,13 +38,21 @@ def scrape_website():
                 div.extract()
 
         # Remove elements matching specific tags
-        for selector in ['header', 'footer', 'aside', 'nav', 'svg', 'ol', 'ul', 'title', 'h1', 'figcaption']:
+        for selector in ['header', 'footer', 'aside', 'nav', 'svg', 'title', 'h1', 'figcaption', 'button']:
             elements = soup.find_all(selector)
             for element in elements:
                 element.extract()
 
+        for text in [heading,'Ad Feedback','Trending', 'Also Read','Sign Up','Get Alerts','Share this on', 'Skip to main content', 'Top news', 'Latest news', 'Follow for more', 'Popular From' ,'opt out at any time', 'more newsletters', 'Privacy Policy', ]:
+            div = soup.find_all(string = re.compile(text, re.IGNORECASE))
+            for i in div:
+                i.parent.extract()
+
         # Get the text content and remove unnecessary whitespaces
-        lines = soup.get_text().split("\n")
+        lines = []
+        for paragraph in soup.find_all('p'):
+            lines.extend(paragraph.get_text().splitlines())
+
         final_text = " ".join([line.strip() for line in lines if line.strip() != ""])
 
         return jsonify({'output': final_text, 'heading': heading})
